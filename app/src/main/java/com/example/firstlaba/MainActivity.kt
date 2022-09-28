3c8d51c62f71b7c566427120a0706d0888c9f657
lorex59 <lorex59@mail.ru>
2022-09-28 12:19:57 +1000

    finish

--- app/src/main/java/com/example/firstlaba/MainActivity.kt
+++ app/src/main/java/com/example/firstlaba/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.firstlaba
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.firstlaba.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding =  ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+        setContentView(binding.root)
+        binding.button.setOnClickListener {
+            increment()
+        }
+    }
+
+    private fun increment() {
+        var temp: Int = binding.textView.text.toString().toInt() + 1
+        binding.textView.text = temp.toString()
     }
 }
